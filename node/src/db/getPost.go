fd66a482abcd7795ddf79f6907fa643c34bb2f20
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2023-08-28 18:00:51 -0600

    no entiendo que carajos ocasiona esto

--- node/src/db/getPost.go
+++ node/src/db/getPost.go
@@ -1,23 +1,23 @@
 package db
 
 import (
-	"database/sql"
-
 	"github.com/ranon-rat/decChan/core"
 )
 
 func GetPosts(name string, date int) (blocks []core.BlockPost) {
 	db := ConnectDB()
-	var r *sql.Rows
+
 	if core.Boards[name] {
-		r, _ = db.Query("SELECT * FROM Posts WHERE board=?1 and date<=?2 LIMIT ?3 ORDER BY date ASC", name, date, core.LimitPerBoard)
+		r, _ := db.Query("SELECT * FROM Posts WHERE board=?1 and date<=?2 LIMIT ?3 ORDER BY date ASC", name, date, core.LimitPerBoard)
+
+		blocks = ScanningPost(r)
 
 	} else {
 		// I use the board as a way of saying "hey this could be a board or a thread and if is a thread just get all this stuff and just that"
-		r, _ = db.Query("SELECT * FROM Posts WHERE board=?1 OR hash=?1 ORDER BY date DESC ", name)
+		r, _ := db.Query("SELECT * FROM Posts WHERE board=?1 OR hash=?1 ORDER BY date DESC ", name)
+		blocks = ScanningPost(r)
 
 	}
-	blocks = ScanningPost(r)
 
 	return
 }
